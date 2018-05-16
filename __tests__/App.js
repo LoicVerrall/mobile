2a37081b304fe2baa63b33e17e2c4635de62b4f7
Loic Verrall <loic.verrall@gmail.com>
2018-05-16 12:28:21 +0100

    Added API unit test

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,3 +1,10 @@
-test('Demo test', () => {
+import * as Api from '../app/lib/Api'
+
+test('Demo test, ensure unit testing is working', () => {
   expect(0).toBe(0)
+})
+
+// Test the API class by retrieving the endpoint URL.
+test('API end point URL is set to server', () => {
+  expect(Api.getEndpoint()).toBe('https://api.uni.ninja/v1')
 })
